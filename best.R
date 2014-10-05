255f4203d98034406aa1151caa8c94512bfb9b75
jpl33 <pladott1@gmail.com>
2014-10-05 07:55:22 +0300

    sort factor as.numeric(as.character)

--- best.R
+++ best.R
@@ -7,20 +7,22 @@ best<-function(state, outcome){
   #"state" parameter check
   stateN<-7
   #"outcome" parameter check
-  if (outcome=="heart attack"){outcomeN<-11}
+  #if (outcome=="heart attack"){outcomeN<-11}
+  outcomeN<-switch(outcome,"heart attack"=11,"heart failure"=17,"pneumonia"=23)
   #read the file into a data frame
-  df<-read.csv("outcome-of-care-measures.csv",stringsAsFactors = FALSE)
+  df<-read.csv("outcome-of-care-measures.csv")#,stringsAsFactors = FALSE)
   #order df according to state,outcome value, hospital name
-  df<-df[order(df[,stateN],df[,outcomeN],df[,"Hospital.Name"]),]
+  #force treatment of outcome as "numeric" instead of "factor"!
+  df<-df[order(df[,stateN],as.numeric(as.character(df[,outcomeN])),df[,"Hospital.Name"]),]
   #create logical vector for rows where "state" column equals "state" parameter
   st<-df[,stateN]==state 
   #get first line of "state" hospitals, get min "outcome" value as benchmark
-  min<-df[st,][1,"Hospital.Name"]
+  min<-as.character(df[st,][1,"Hospital.Name"])
   min
   
 }
 
-# z1<-c(1,2,3,4,5,6,7,1)
+# z1<-factor(c(1,2,3,4,5,6,7,1))
 # z2<-c("il","il","az","tx","az","tx","tx","il")
 # z3<-c("amka","zfat","jones","sloan","morgan","bendict","carter","afula")
 # tst<-cbind(z1,z2,z3)
